f9b4629761deda4858814910c1b6bad5ae08edd5
root <root@cogtop>
2015-10-13 21:31:42 -0700

    first commit

---
+++ simonsays.ino
@@ -0,0 +1,106 @@
+static int pins[] = {10,9,8,11}; //up, right, down, left
+static int tones[] = {247, 523, 988, 1397};
+int tonePin = 12;
+int upDownPin = 0;
+int upDown = 512;
+int leftRightPin = 1;
+int leftRight = 512;
+int next = 0;
+boolean lost = false;
+int moves[20];
+int roundNum = 0;
+
+void setup(){
+  for (int i = 0; i < 4; i++){
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);
+  }
+  delay(2000);
+  for (int i = 0; i < 4; i++){
+    digitalWrite(pins[i], LOW);
+  }  
+  delay(1000);
+}
+
+void flash(){
+  for (int n = 0; n < 4; n++){
+    for (int i = 0; i < 4; i++){
+      digitalWrite(pins[i], HIGH);
+    }
+    tone(tonePin, tones[0], 500);
+    delay(500);
+    for (int i = 0; i < 4; i++){
+      digitalWrite(pins[i], LOW);
+    }  
+    delay(500);
+  }
+}
+
+void loop(){
+  randomSeed(analogRead(5));
+  while(!lost){
+    next = random(0,4);
+    moves[roundNum] = next;
+    roundNum++;
+    for (int i = 0; i < roundNum; i++){    //play moves
+      digitalWrite(pins[moves[i]], HIGH);
+      tone(tonePin, tones[moves[i]], 500);
+      delay(500); 
+      digitalWrite(pins[moves[i]], LOW);
+      delay(200);
+    }
+    delay(800);
+    for (int i = 0; i < roundNum; i++){    //user plays round
+      while(true){    //wait for input
+        upDown = analogRead(upDownPin);
+        leftRight = analogRead(leftRightPin);       
+        //evaluate input
+        if(upDown > 600){    //User inputs up
+          digitalWrite(pins[0], HIGH);
+          tone(tonePin, tones[0], 300);
+          delay(300);
+          digitalWrite(pins[0], LOW);
+          if(moves[i] != 0){
+            lost = true;
+            break;
+          }
+        }
+        else if(leftRight > 600){    //User inputs right
+          digitalWrite(pins[1], HIGH);
+          tone(tonePin, tones[1], 300);
+          delay(300);
+          digitalWrite(pins[1], LOW);
+          if(moves[i] != 1){
+            lost = true;
+          }         
+          break;
+        }
+        else if(upDown < 400){      //user inputs down
+          digitalWrite(pins[2], HIGH);
+          tone(tonePin, tones[2], 300);
+          delay(300);
+          digitalWrite(pins[2], LOW);
+          if(moves[i] != 2){
+            lost = true;
+          }         
+          break;
+        }
+        else if(leftRight < 400){    //user inputs left
+          digitalWrite(pins[3], HIGH);
+          tone(tonePin, tones[3], 300);
+          delay(300);
+          digitalWrite(pins[3], LOW);
+          if(moves[i] != 3){
+            lost = true;
+          }       
+          break;
+        }
+      }
+      delay(1000);
+    }
+  }
+  flash();
+  delay(10000);    //delay between games
+  lost = false;
+  roundNum = 0;
+}
